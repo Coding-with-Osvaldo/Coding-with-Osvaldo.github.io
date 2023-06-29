66eda71a673532810313d5beadc0995079169ebf
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-06-29 11:11:34 -0300

    feat #3: Basic styling of the presentation page completed

--- js/presentation.js
+++ js/presentation.js
@@ -3,5 +3,8 @@ fetch("https://ads-grades-api.onrender.com/grade/13", {
 })
     .then(res => res.json())
     .then(res => {
+        document.querySelector("#teacher_name").innerText = res.Teacher.name
+        document.querySelector("div#teacher_information img").src = res.description.img
+        document.querySelector("#grade_title").innerText = res.description.name
         timedWriting(res.description.description, document.querySelector("p#description_content"), 1)
     })
